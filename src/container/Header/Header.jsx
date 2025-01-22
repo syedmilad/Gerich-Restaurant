9a86845e9de7f045fd48c55eacf1a8535de6cd60
syedmilad <syedmilad99@gmail.com>
2025-01-23 00:41:39 +0500

    addd some classes

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
 
 import './Header.css';
+import SubHeading from "../../components/SubHeading/SubHeading"
 
+const buttonStyle = {
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  minHeight: "100vh",
+
+  padding: "4rem 6rem"
+};
+
+const app__wrapper__info = {
+  flex: "1",
+  width: "100%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "flex-start",
+  flexDirection: "column",
+  border: "1px solid black"
+
+}
+const app__wrapper__img = {
+  flex: "1",
+  width: "100%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  marginLeft: "2rem",
+  border: "1px solid black"
+}    
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header" style={buttonStyle} id="home">
+    <div style={app__wrapper__info}>
+      <SubHeading/>
+    </div>
+    <div style={app__wrapper__img}>asfsda</div>
   </div>
 );
 
