ffb552a76e76fd942988726eea46851f5f1c7644
syedmilad <syedmilad99@gmail.com>
2025-02-07 12:31:12 +0500

    added the footer component with non completed.

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-
-import './Footer.css';
+import React from "react";
+import { images } from "../../constants";
+import { FiInstagram, FiTwitter, FiFacebook } from "react-icons/fi";
+import { FooterOverlay, Newsletter } from "../../components";
+import "./Footer.css";
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding">
+    <FooterOverlay />
+    <Newsletter />
   </div>
 );
 
